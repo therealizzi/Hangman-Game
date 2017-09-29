d7a7a512a31979fa552700489a1b04e521f13ba7
TheRealIzzi <israel.peck@gmail.com>
2017-09-29 16:46:21 -0500

    Basic game works, needs reset functionality... 90%

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,27 @@
 
-
 //this sets global variables
+
+//this is the win counter
 	var wins = 0;
 	document.getElementById("wins").innerHTML = wins;
 
+//this is the tries counter
 	var tries = 12;
 	document.getElementById("tries").innerHTML = tries;	
 
+//this array holds the random words
 	var randWordArray = ["ASTROIDS", "FROGGER", "PONG", "CASTLEVANIA", "ZELDA", "TRON", "DOOM", "CENTIPEDE", "PAPERBOY", "EXCITEBIKE", "PACMAN","SONIC","CONTRA","TETRIS"]
 
 //this generates a random word 
 	var randWord = randWordArray[Math.floor(Math.random()*randWordArray.length)];
 
-//this turns the random word into a string in variable "letters"
+//this turns the random word into a character string "letters"
 	var letters = randWord.split("");
 
-//this variable holds the wrong letters
+//this variable holds the incorrect guesses
 	var wrongLetters = [];
 
-//this creates a new variable "progress" that has blanks for each letter in the string
+//this variable tracks each letter in the string
 	var progress = [];
 	
 	for (i = 0; i < randWord.length; i++){	
@@ -29,18 +32,16 @@ console.log(randWord)
 console.log(letters)
 console.log(progress)
 
-//this creates a 'guess' variable every time a key is pressed
+//this function creates a 'guess' variable every time an event key is pressed
 
 document.onkeydown = function(event){
 var guess = String.fromCharCode(event.keyCode);
 
 var flag = false;
 
-
-//this splices the progress array and adds in the correct guess
+//this identifies and processes correct guess
 
 	for (var i=0; i<letters.length; i++){
-
 		if (letters[i] == guess){
 			flag = true;
 			progress.splice(i,1,guess);
@@ -49,9 +50,10 @@ var flag = false;
 		document.getElementById("progress").innerHTML = progress.join(' ');	
 			}
 		}
-			
-	if(flag === false){
-		console.log(guess)
+
+//this identifies and processes incorrect guesses		
+
+	if (flag === false){
 		wrongLetters.push(guess);
 		document.getElementById("wrongletters").innerHTML = wrongLetters.join('');
 		tries --;
@@ -61,32 +63,22 @@ var flag = false;
 		document.getElementById("wrongletters").innerHTML = "You lose!";
 		}
     }
+
+//this identifies and processes the 'wins' 
+var losing = true;
+
+	for (var i=0; i<progress.length; i++){	
+		if (progress[i] === "_"){
+			losing = false;
+			console.log(losing)
+		} 
+	}
+
+	if (losing === true){
+		console.log("You won!")
+		wins ++;
+		document.getElementById("tries").innerHTML = "You won!";
+		document.getElementById("wrongletters").innerHTML = "You won!";
+		document.getElementById("wins").innerHTML = wins;
+	}
 }
-
-//this ends the wrongLetters variable at 12
-
-// 	if (wrongLetters.length > 12){
-		
-// 		document.getElementById("wrongletters").innerHTML = "You LOSE!"
-// 	}
-
-// 	if(letters.includes(wrongLetters)){
-
-// 		tries +=1;	
-
-// //this reduces the count of guesses remaining 
-// 	if (tries >= 1) {
-
-// 			tries -= 1
-// 	}
-	
-// 	else {
-// 		tries = "You Lose!"
-// 	};
-	
-// 	document.getElementById("tries").innerHTML = tries;
-// 	}
-
-// //this is the alphabet (delete if don't use)
-// var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-
