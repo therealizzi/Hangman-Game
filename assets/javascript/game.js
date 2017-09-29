1c86744b778f16acf16f518d9f27848cd59881d1
TheRealIzzi <israel.peck@gmail.com>
2017-09-29 09:27:45 -0500

    organizing js, still ironing out lots of bugs, ~70%

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,68 +3,63 @@
 	window.onload=function(){
 
 //this sets global variables
-	var wins = "0";
+	var wins = 0;
 	document.getElementById("wins").innerHTML = wins;
 
 	var tries = 12;
 	document.getElementById("tries").innerHTML = tries;
 
-//this shows which key was pressed
-
-	document.onkeydown = function(event){
-	var guessed = String.fromCharCode(event.keyCode);
-
-	console.log(guessed)
-	var indexGuess = randWord.indexOf(guessed);
-
-	console.log(indexGuess)
-
-//this checks guess against randWord
-
-	if(letters.includes(guessed)){
-		console.log("You guessed right!");
-	} else {
-	document.getElementById("guessed").innerHTML += guessed;	
-	}
-
-//this splices the progress array and adds in the correct guess
-	var progress
-
-	var guessed 
-	
-	blanks.splice(indexGuess,1,guessed);
-
-	console.log(blanks)
-
-	document.getElementById("progress").innerHTML = blanks.join(" ");
-
-//this reduces the count of "tries" with each click
-
-	if (tries > 0) {tries -= 1}
-		else {tries = "You Lose!"};
-	document.getElementById("tries").innerHTML = tries;
-	}
-
 //this generates a random word 
-
-	var randWordArray = ["ASTROIDS", "FROGGER", "PONG", "DOOM", "CENTIPEDE", "PAPERBOY", "EXCITEBIKE", "PACMAN","SONIC","CONTRA","TETRIS"]
+	var randWordArray = ["ASTROIDS", "FROGGER", "PONG", "CASTLEVANIA", "ZELDA", "TRON", "DOOM", "CENTIPEDE", "PAPERBOY", "EXCITEBIKE", "PACMAN","SONIC","CONTRA","TETRIS"]
 
 	var randWord = randWordArray[Math.floor(Math.random()*randWordArray.length)];
 
-//this turns the random word into a string of unknowns
-
+//this turns the random word into a string of letters
 	var letters = randWord.split("");
-	var progress = "";
 
-	for (u = 0; u < randWord.length; u++){
-		progress += "_ "
-	}
+//this creates a new variable that has blanks for each letter in the string
+	var progress = "";
 	
+	for (u = 0; u < randWord.length; u++){	
+		progress += "_ "}
+	
+	document.getElementById("progress").innerHTML = progress;
+
+//this creates a variable of blank letters
 	var blanks = progress.split()
 
-//this swaps blanks for letters
+//this is the keydown event function when a user chooses a letter
+	document.onkeydown = function(event){
+	
+//this creates the guess variable
+	var guess = String.fromCharCode(event.keyCode);
 
-	document.getElementById("progress").innerHTML = progress;
+//this checks the guess against letters in the random word
+	if(letters.includes(guess)){
+
+		tries +=1;
+	
+	//this splices the progress array and adds in the correct guess
+		var indexGuess = randWord.indexOf(guess);
+	
+		blanks.splice(indexGuess,1,guess);
+
+		document.getElementById("progress").innerHTML = blanks.join(" ");
+
+		} else {
+		document.getElementById("guess").innerHTML += guess;	
+		}
+
+//this ends the guess variable at 12 
+	if (guess.length > 12){
+		document.getElementById("guess").innerHTML = "You LOSE!"
+	}
+
+//this reduces the count of guesses remaining 
+	if (tries >= 1) {tries -= 1}
+		else {tries = "You Lose!"};
+	document.getElementById("tries").innerHTML = tries;
+	}
 
 //these are console logs
 	console.log(blanks);
@@ -73,5 +68,4 @@
 
 //this is the alphabet (delete if don't use)
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-
-}
+	}
