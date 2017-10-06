7f72efca566885b021eaeebc2d36757583b13658
TheRealIzzi <israel.peck@gmail.com>
2017-10-06 09:54:55 -0500

    Making edits re: Jay's comments, fixing indentation

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,31 @@
 //Global variables
-	var wins = 0;
-	document.getElementById("wins").innerHTML = wins;
+var wins = 0;
+document.getElementById("wins").innerHTML = wins;
 
-//this is SUPPOSED to be the game function
-	function myGame(){
-	}
 //this is the tries counter
-	var tries = 12;
-	document.getElementById("tries").innerHTML = tries;	
+var tries = 12;
+document.getElementById("tries").innerHTML = tries;	
 
 //this array holds the random words
-	var randWordArray = ["ASTROIDS", "FROGGER", "PONG", "CASTLEVANIA", "ZELDA", "TRON", "DOOM", "CENTIPEDE", "PAPERBOY", "EXCITEBIKE", "PACMAN","SONIC","CONTRA","TETRIS"]
+var randWordArray = ["ASTROIDS", "FROGGER", "PONG", "CASTLEVANIA", "ZELDA", "TRON", "DOOM", "CENTIPEDE", "PAPERBOY", "EXCITEBIKE", "PACMAN","SONIC","CONTRA","TETRIS"]
 
 //this generates a random word 
-	var randWord = randWordArray[Math.floor(Math.random()*randWordArray.length)];
+var randWord = randWordArray[Math.floor(Math.random()*randWordArray.length)];
 
 //this turns the random word into a character string "letters"
-	var letters = randWord.split("");
+var letters = randWord.split("");
 
 //this variable holds the incorrect guesses
-	var wrongLetters = [];
+var wrongLetters = [];
 
 //this variable tracks each letter in the string
-	var progress = [];
+var progress = [];
+
+for (i = 0; i < randWord.length; i++){	
+	progress.push("_");
+}
 	
-	for (i = 0; i < randWord.length; i++){	
-		progress.push("_");
-	}
-	document.getElementById("progress").innerHTML = progress.join('');
+document.getElementById("progress").innerHTML = progress.join('');
 
 console.log(randWord)
 console.log(letters)
@@ -42,45 +40,44 @@ var flag = false;
 
 //this identifies and processes correct guess
 
-	for (var i=0; i<letters.length; i++){
-		if (letters[i] == guess){
-			flag = true;
-			progress.splice(i,1,guess);
+for (var i=0; i<letters.length; i++){
+	if (letters[i] == guess){
+		flag = true;
+		progress.splice(i,1,guess);
 		console.log(letters[i]);
 		console.log(progress);
 		document.getElementById("progress").innerHTML = progress.join(' ');	
-			}
-		}
+	}
+}
 
 //this identifies and processes incorrect guesses		
 
-	if (flag === false){
-		wrongLetters.push(guess);
-		document.getElementById("wrongletters").innerHTML = wrongLetters.join('');
-		tries --;
-		document.getElementById("tries").innerHTML = tries;	
+if (flag === false){
+	wrongLetters.push(guess);
+	document.getElementById("wrongletters").innerHTML = wrongLetters.join('');
+	tries --;
+	document.getElementById("tries").innerHTML = tries;	
 	if (tries < 1){
 		document.getElementById("tries").innerHTML = "You lose!";
 		document.getElementById("wrongletters").innerHTML = "You lose!";
-		}
-    }
+	}
+}
 
 //this identifies and processes the 'wins' 
 var solved = true;
 
-	for (var i=0; i<progress.length; i++){	
-		if (progress[i] === "_"){
-			solved = false;
-			console.log(solved)
-		} 
-	}
+for (var i=0; i<progress.length; i++){	
+	if (progress[i] === "_"){
+		solved = false;
+		console.log(solved)
+	} 
+}
 
-	if (solved === true){
-		console.log("You won!")
-		wins += 1;
-		document.getElementById("tries").innerHTML = "You won!";
-		document.getElementById("wrongletters").innerHTML = "You won!";
-		document.getElementById("wins").innerHTML = wins;
-		document.onkeydown = myGame();
-	}
-	}
+if (solved === true){
+	console.log("You won!")
+	wins += 1;
+	document.getElementById("tries").innerHTML = "You won!";
+	document.getElementById("wrongletters").innerHTML = "You won!";
+	document.getElementById("wins").innerHTML = wins;
+	document.onkeydown = myGame();
+}
